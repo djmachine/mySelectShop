56514b4f0020209eb6bba6ed342b367cb080a96d
[IoT3] 김대중 <qw1310@naver.com>
2025-02-11 11:11:44 +0900

    회원의 상품 조회와, admin의 상품 조회 기능을 구현했습니다.

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
@@ -34,11 +35,16 @@ public class Product extends Timestamped {
     @Column(nullable = false)
     private int myprice;
 
-    public Product(ProductRequestDto requestDto) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Product(ProductRequestDto requestDto, User user) {
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
         this.link = requestDto.getLink();
         this.lprice = requestDto.getLprice();
+        this.user = user;
     }
 
     public void update(ProductMypriceRequestDto requestDto) {
